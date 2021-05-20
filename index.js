bddefe871046a925914eac206da85d88891fc9e4
sparkandflash <47806016+sparkandflash@users.noreply.github.com>
2021-05-20 10:18:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -59,7 +59,17 @@ else if(!msg.guild){
 	 msg.channel.send(suffix);
 	
  
-	}
+	}else if(msg.content.startsWith(prefix+"kiss"))
+		 {
+    const Discord = require('discord.js');
+    var usertokiss = msg.mentions.users.first();
+    //msg.channel.send(`${user} kisses ${usertokiss}\n https://tenor.com/view/kiss-anime-love-gif-4958649`);
+    //https://tenor.com/view/kiss-anime-love-gif-4958649
+    const Embed = new Discord.RichEmbed().setColor('#06cef1')
+    Embed.setTitle(msg.author.tag+ ` kisses `+usertokiss.tag)
+    Embed.setImage(`https://cdn.discordapp.com/attachments/814551742754324512/814581759857524776/unknown.gif`) 
+    msg.channel.send(Embed);
+}
 	else if(msg.content.startsWith(prefix+"setn"))
 		{
     if (!devs){
